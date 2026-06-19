ab6372d640c7a8a4388a1f950c4614242764ad39
Rahul A R <arrahul465@gmail.com>
2026-06-19 14:41:35 +0530

    Time: 386 ms (31.94%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,23 +1,22 @@
 -- Write your PostgreSQL query statement below
-SELECT 
-  p1.product_id,
-  COALESCE(p2.new_price, 10) AS price
-FROM (
-  SELECT DISTINCT product_id 
-  FROM Products
-) p1
-LEFT JOIN (
-  SELECT 
-    product_id, 
-    new_price,
-    change_date
-  FROM Products
-  WHERE change_date <= '2019-08-16'
-) p2 ON p1.product_id = p2.product_id
-  AND p2.change_date = (
-    SELECT MAX(change_date)
-    FROM Products p3
-    WHERE p3.product_id = p1.product_id
-      AND p3.change_date <= '2019-08-16'
-  )
-ORDER BY p1.product_id;
+SELECT
+    product_id,
+    10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16'
+
+UNION
+
+SELECT
+    product_id,
+    new_price AS price
+FROM Products
+WHERE (product_id, change_date) IN (
+    SELECT
+        product_id,
+        MAX(change_date)
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+);
